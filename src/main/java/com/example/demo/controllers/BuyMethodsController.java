2800334da848aa50074516b616b9485cb220ddc0
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-06-08 00:23:17 +0300

    Update BuyMethodsController.java

--- src/main/java/com/example/demo/controllers/BuyMethodsController.java
+++ src/main/java/com/example/demo/controllers/BuyMethodsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("buy-methods")
+@RequestMapping("/buy")
 public class BuyMethodsController {
     @GetMapping
     public String getBuyMethods(){
