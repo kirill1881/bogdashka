b9c21e7954729cd6726a2754134af245b7e335a6
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-08-24 17:05:51 +0300

    Update MainController.java

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -8,13 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class MainController {
-     @Autowired
-    CountRepo countRepo;
+    
     @GetMapping
     public String getMainPage(){
-         Cont cont = new Cont();
-        cont.setCount("500");
-        countRepo.save(cont);
+         
 
         return "index";
     }
